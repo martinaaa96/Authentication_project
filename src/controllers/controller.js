0e4e9c739a18275659514fee1c8eabf64f73d1dc
Martina Vangelova <mvangelovaa@abv.bg>
2023-08-05 17:07:49 +0300

    show home page

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,11 +1,18 @@
+//exports.getHomePage = (req,req)=>{
+    //res.render('index')
+//}
 
+//exports.getLoginPage = (req,res)=>{
 
-exports.getLoginPage = (req,res)=>{
+   // res.render('login')
+//}
 
-    res.render('login')
-}
+//exports.getRegisterPage = (req,res)=>{
+  //  res.render('register');
 
-exports.getRegisterPage = (req,res)=>{
-    res.render('register');
-    
-}
+//}
+
+//exports.getErrorPage= (req,res)=>{
+  //  res.render('404');
+
+//}
--- src/index.js
+++ src/index.js
@@ -9,8 +9,18 @@ const app = express();
 
 setupViewEngine(app);
 
-app.use(express.static('src/public'));
+app.use(express.static('./src/public'));
 app.use(express.urlencoded({extended: false}));
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+app.get('/login',(req,res)=>{
+    res.render('login')
+});
+
+app.get('/register', (req,res)=>{
+    res.redirect('register')
+})
 
 initDataBase()
 .then(()=> app.listen(config.PORT, ()=> console.log(`Server is listening on port ${config.PORT}...`)))
